ad7802c31ac5f9d3aaa3b60728f74efbf7d9d321
alpha-noyan <hammadbaseer4@gmail.com>
2026-08-02 14:26:08 +0500

    all set

--- template/routes/authRoutes.js
+++ template/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const { registerUser, loginUser, forgetPassword, resetPassword } = require('../conrtollers/userController');
+const { registerUser, loginUser, forgetPassword, resetPassword } = require('../controllers/userController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
